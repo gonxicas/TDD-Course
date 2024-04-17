075dccc3ba9f4e2e8375ed2b76f32bc9135ef15e
Gonzalo Casin <gcasinbricio@hotmail.com>
2024-04-17 10:26:19 +0200

    feat(money.cpp): check for type

--- src/Money.cpp
+++ src/Money.cpp
@@ -1,6 +1,6 @@
 #include "Money.h"
-
+#include <typeinfo>
 bool Money::operator==(const Money& money) const
 {
-    return amount == money.amount;
+    return amount == money.amount && typeid(*this).name() == typeid(money).name();
 }
